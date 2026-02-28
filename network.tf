397ce7f923bb7853a41e1786f6110e316ba1def9
DirtyPatel <pate0323@algonquincollege.com>
2026-02-27 20:37:37 -0500

    added variables, network and resource group

---
+++ network.tf
@@ -0,0 +1,40 @@
+
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.labelPrefix}-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.labelPrefix}-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.labelPrefix}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.labelPrefix}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.labelPrefix}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "labelPrefix" {
+  description = "Your college username."
+  type        = string
+  default = "pate0323"
+}
+
+variable "region" {
+  description = "region where you have OCI tenancy"
+  type        = string
+  default = "eastus"
+}
+
+variable "admin_username" {
+  type        = string
+  default = "azureadmin"
+}
